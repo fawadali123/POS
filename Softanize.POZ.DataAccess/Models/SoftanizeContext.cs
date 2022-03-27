5668454c2dd4476a71c80906526298837c55c399
HP <HP@DESKTOP-NLGU6BV>
2022-03-27 11:12:55 +0500

    Add project files.

---
+++ Softanize.POZ.DataAccess/Models/SoftanizeContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Softanize.POZ.DataAccess.Models
+{
+    public partial class SoftanizeContext : IdentityDbContext
+    {
+
+        public SoftanizeContext(DbContextOptions<SoftanizeContext> options)
+            : base(options)
+        {
+        }
+
+
+    }
+}
